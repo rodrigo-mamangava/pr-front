873a1206d5702abf2ca5d70a4274ec38042fc450
rodrigo-mamangava <rodrigo@mamangava.com>
2016-07-29 18:42:17 -0300

    primeiro push

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Ponto de Referência</title>
+
+        <!-- Bootstrap -->
+        <link href="css/main.css" rel="stylesheet">
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <header>
+
+            <?php include './template-parts/menu.php'; ?>
+
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="capa">
+
+                        <img src="image/capa.png">                                        
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="capa-titulo">
+                            <h1>MRA, Movimento Rio Atende.</h1>
+                            <p>Qual a verdadeira medalha que o Brasil deve ganhar nas Olimpíadas?</p>
+                        </div><!-- .capa-titulo -->
+                    </div>
+                </div>
+            </div>
+
+        </header>
+
+        <main>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+
+                        <h2>Quero implementar</h2>
+                        <p class="descricao">Projetos de consultoria</p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">                        
+                        <a class="btn-home-1">Movimento GAS</a>
+                    </div>
+                    <div class="col-sm-3">                        
+                        <a class="btn-home-1">Movimento GAS</a>
+                    </div>
+                    <div class="col-sm-3">                        
+                        <a class="btn-home-1">Movimento GAS</a>
+                    </div>
+                    <div class="col-sm-3">                        
+                        <a class="btn-home-1">Movimento GAS</a>
+                    </div>
+                    <div class="col-sm-3">                        
+                        <a class="btn-home-1">Movimento GAS</a>
+                    </div>
+                    <div class="col-sm-3">                        
+                        <a class="btn-home-1">Movimento GAS</a>
+                    </div>
+                    <div class="col-sm-3">                        
+                        <a class="btn-home-1">Movimento GAS</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </main>
+
+        <footer>
+            <p>footer</p>
+        </footer>
+
+
+
+
+    </body>
+</html>
